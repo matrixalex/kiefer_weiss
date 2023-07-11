91153d71c7f20f3fb23604f239f7a01d6359460b
Mary Puryasseva <MaryPuryasseva@yandex.ru>
2023-07-11 14:03:45 +0300

    popitka

---
+++ bernoulli/Masha_11.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 11 12:22:33 2023
+
+@author: User
+"""
+
+from scipy.stats import binom
+import numpy as np
+
+def pmf(sample_number,probability): 
+    """ binomial probability mass function"""
+    success_number = np.arange(0,sample_number+1)
+    return binom(success_number,sample_number,probability)
+
+def modified_kw(horizon,lam0,lam1,theta0,theta1,probability):
+    lagr = np.empty((horizon+1, horizon+1))
+    lagr[:] = np.nan
+     
+    accept = np.zeros((horizon+1, horizon+1), dtype=bool)
+    cont = np.zeros((horizon+1, horizon+1), dtype=bool)
+    
+    prob0 = pmf(horizon, th0)
+    prob1 = pmf(horizon, th1)
+    
+    lagr[horizon] = np.minimum(lam0*prob0, lam1*prob1)
+    accept[horizon] = lam0*prob0>=lam1*prob1
+    cont[horizon] = np.zeros(horizon, dtype = bool)
+    
+    if H>1:
+        for n in np.flip(np.arange(horizon)):
+            prob0 = pmf(n, th0)
+            prob1 = pmf(n, th1)
+            lagr_n = np.minimum(lam0*prob0, lam1*prob1)
+            x = np.arange(n+2)
+            
+            h = lagr[n+1]/(n+1)*(n+1-x)
+            
+    
+   
+    
