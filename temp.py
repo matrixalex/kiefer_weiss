01b9530c1af3dac2d258e27bdff304add4faca3a
Mary Puryasseva <MaryPuryasseva@yandex.ru>
2023-07-11 12:15:06 +0300

    change comment in temp.py

--- temp.py
+++ temp.py
@@ -5,4 +5,13 @@ Spyder Editor
 This is a temporary script file.
 """
 
-fffff
+from scipy.stats import binom
+import numpy as np
+
+def pmf(sample_number,probability):
+    
+    """ binomial probability mass function"""
+    
+    success_number = np.arange(0,sample_number+1)
+    return binom(success_number,sample_number,probability)
+    
