70c7db6a47042650d9126e3ccbda71de19c4d0a1
Mary Puryasseva <MaryPuryasseva@yandex.ru>
2023-07-12 12:33:06 +0300

    remod

--- bernoulli/Masha_11.py
+++ bernoulli/Masha_11.py
@@ -8,40 +8,38 @@ Created on Tue Jul 11 12:22:33 2023
 from scipy.stats import binom
 import numpy as np
 
-def pmf(sample_number,probability): 
+def pmf(sample_number,th): 
     """ binomial probability mass function"""
     success_number = np.arange(0,sample_number+1)
-    return binom(success_number,sample_number,probability)
+    return binom(success_number,sample_number,th)
 
-def modified_kw(horizon,lam0,lam1,theta0,theta1,probability):
-    lagr = np.empty((horizon+1, horizon+1))
-    lagr[:] = np.nan
-     
+def modified_kw(horizon,lam0,lam1,th0,th1,th):
+    lagr = np.full((horizon+1, horizon+1),np.nan)
     accept = np.zeros((horizon+1, horizon+1), dtype=bool)
     cont = np.zeros((horizon+1, horizon+1), dtype=bool)
     
-    prob0 = pmf(horizon, th0)
-    prob1 = pmf(horizon, th1)
+    z0 = pmf(horizon, th0)
+    z1 = pmf(horizon, th1)
     
-    lagr[horizon] = np.minimum(lam0*prob0, lam1*prob1)
-    accept[horizon] = lam0*prob0>=lam1*prob1
+    lagr[horizon] = np.minimum(lam0*z0, lam1*z1)
+    accept[horizon] = lam0*z0>=lam1*z1
     cont[horizon] = np.zeros(horizon, dtype = bool)
     
-    if H>1:
+    if horizon>1:
         for n in np.flip(np.arange(horizon)):
-            prob0 = pmf(n, th0)
-            prob1 = pmf(n, th1)
-            lagr_n = np.minimum(lam0*prob0, lam1*prob1)
+            z0 = pmf(n, th0)
+            z1= pmf(n, th1)
+            lagr_n = np.minimum(lam0*z0, lam1*z1)
             x = np.arange(n+2)
             
             h = (lagr[n+1]/(n+1)*(n+1-x))[:(n+1)]
-            t = (lagr[n+1]/(n+1)*(x))[(n+1):]
+            t = (lagr[n+1]/(n+1)*(x))[(-(n+1)):]
             
             lagr[n] = np.minimum(pmf(n, th) + h + t, lagr_n)
-            accept[n] = lam0*prob0>=lam1*prob1
+            accept[n] = lam0*th0>=lam1*th1
             cont[n] = lagr[n]<lagr_n
             
-    return(np.concatenate(cont,accept),axis=0)
+    return(cont,accept)
             
     
             
