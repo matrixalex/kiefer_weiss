27a419703ca315a599a5e137de5d3766b89760a1
SIPLIbY <oki_cs_228@mail.ru>
2023-07-17 14:04:57 +0700

    fixed optimizers in original_kw function, but need to check their correctness

--- bernoulli/original_kw.py
+++ bernoulli/original_kw.py
@@ -1,12 +1,12 @@
 import scipy.optimize
 import scipy.stats
 from typing import Tuple
-
+from scipy.optimize import differential_evolution
 from asn import average_sample_number
 from core import modified_kw
 from utils import horizon_bound
 from config import DEFAULT_OPTIMIZATION_PRECISION_LEVEL, MAX_OPTIMIZATION_COUNT
-
+import time
 
 def negative_average_sample_number(x, cont):
     """Negative ASN for maximization in optimizer."""
@@ -25,23 +25,42 @@ def original_kw(lam0, lam1, th0, th1, tol=DEFAULT_OPTIMIZATION_PRECISION_LEVEL, 
         if horizon is None:
             horizon = horizon_bound(lam0, lam1, th0, th1, x)
         print(horizon, lam0, lam1, th0, th1, x)
+        horizon = 102
+        print('Modified_kw started')
+        start = time.time()
         cont, accept = modified_kw(horizon, lam0, lam1, th0, th1, x)
-        # TODO: in ASN func first argument must be x!!! This is necessary for optimizer
-        # TODO: add '-' before func, now func calculates minimum value
+        print('Modified_kw finished with time', time.time()-start)
+
+
+        print("ASN started")
+
+        start = time.time()
+
         ASN_old = average_sample_number(x, cont)
 
-        x_max, objective_value, funcalls = scipy.optimize.golden(
-            negative_average_sample_number,
-            args=(cont,),
-            brack=(th0, th1),
-            tol=tol,
-            full_output=True,
-            maxiter=MAX_OPTIMIZATION_COUNT
-        )
+        print("ASN finished with time", time.time() - start)
+
+        #creating bounds
+        bounds = [(th0, th1)]
+
+        print("Optimizer differential_evolution started")
+        start = time.time()
+        result = differential_evolution(negative_average_sample_number, args=(cont,), bounds=bounds)
+
+        #optimizer returns dict, so we need to unpack it
+        objective_value = result['fun']
+        print("Optimizer differential_evolution finished wotk with time", time.time() - start)
+        print("Result", result)
+
 
         return objective_value - ASN_old
 
-    x_min, objective_value, func_call = scipy.optimize.golden(delta, brack=(
-        th0, th1), tol=tol, full_output=True, maxiter=MAX_OPTIMIZATION_COUNT)
 
+    print("Golden started")
+    start = time.time()
+    x_min, objective_value, func_call = scipy.optimize.golden(delta, brack=(
+        th0+tol, th1-tol), tol=tol, full_output=True, maxiter=MAX_OPTIMIZATION_COUNT)
+    print("Golden ended work with time", time.time() - start)
     return x_min, objective_value
+
+print(original_kw(157.696751972207, 193.349705609267, 0.05, 0.15))
