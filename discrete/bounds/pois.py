f301bc752356c968311abf453c203457ffe13631
Mary Puryasseva <MaryPuryasseva@yandex.ru>
2023-07-19 10:42:12 +0300

    created bounds for pois

---
+++ discrete/bounds/pois.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+def hbound(l0,l1,th0,th1,th):
+    res = np.floor(
+        (np.log(l0) / (np.log(th) - np.log(th0)) + np.log(l1) /  (np.log(th1) - np.log(th))) /
+    ((th1 - th)/(np.log(th1) - np.log(th)) - (th - th0)/(np.log(th) - np.log(th0)))
+        )
+    return res
+
+def ubound(n,l0,th0,th):
+    res = np.floor(
+        np.log(l0) / np.log(th / th0) + n * (th - th0) / np.log(th / th0)
+        )
+    return res
+
+def lbound(n, l1, th1, th):
+    res =  np.ceil(
+        max(
+            0,
+            -np.log(l1) / np.log(th1 / th) + n * (th1 - th) / np.log(th1 / th)
+            )
+        )
+    return(res)
