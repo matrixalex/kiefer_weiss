fe64a1151364b2e797337ecb20826c079293b9f4
Rustamov Murad <rusmurad@gmail.com>
2023-07-19 15:25:52 +0700

    rename function in binom

--- discrete/bounds/binom.py
+++ discrete/bounds/binom.py
@@ -3,14 +3,14 @@ from math import floor, ceil
 
 size = 1
 
-def Ubound(n, l0, th0, th):
+def ubound(n, l0, th0, th):
     return floor((-np.log(l0) - n * size * np.log((1 - th0)/(1 - th)))/(np.log(th0/th * (1 - th)/(1 - th0))))
 
 
-def Lbound(n, l1, th1, th):
+def lbound(n, l1, th1, th):
     return max(0, ceil((-np.log(l1) - n * size * np.log((1 - th1)/(1 - th)))/(np.log(th1/th * (1 - th)/(1 - th1)))))
 
-def Hbound(l0, l1, th0, th1, th):
+def hbound(l0, l1, th0, th1, th):
     return floor((np.log(l1)/np.log(th1/th * (1 - th)/(1 - th1)) - np.log(l0)/np.log(th0/th * (1 - th)/(1 - th0))) /
                  (np.log((1 - th0)/(1 - th))/np.log(th0/th * (1 - th)/(1 - th0)) -
                  np.log((1 - th1)/(1 - th))/np.log(th1/th * (1 - th)/(1 - th1))
