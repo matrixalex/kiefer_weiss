f750a013cda2bc8f1655f8513928d212dc576193
Mary Puryasseva <MaryPuryasseva@yandex.ru>
2023-07-19 12:37:19 +0300

    create prob func for pois

--- discrete/utils/pois.py
+++ discrete/utils/pois.py
@@ -1,28 +1,23 @@
 from scipy.stats import poisson
-import math
 
 def pmf(s, n, th):
-    res = poisson.pmf(s, n, th)
+    res = poisson.pmf(s, n*th)
     return res
 
 def quant(p, n, th):
-    res = poisson.ppf(p, n, th)
+    res = poisson.ppf(p, n*th)
     return res
 
 def cdf(s, n, th):
-    res = poisson.cdf(s,n,th)
+    res = poisson.cdf(s,n*th)
     return res
 
-def rngen(th,n):
+def rngen(n,th):
     res =  poisson.rvs(th,size = n)
     return res
 
-def d(n, s, x):
-    if n ==1:
-        return (1)
-    else:
-        res = math.comb(x+s,s)* (1 - 1/n)**s/n**x
-        return  res
+
+
         
 
 
