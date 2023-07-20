c3e04f640e6b8ab2c3cf66b0a83b2660ad9acc84
Alexander Stepanov <matrixalex96@gmail.com>
2023-07-20 17:08:41 +0300

    solve_modified debbuged and fixed

--- discrete/utils/core.py
+++ discrete/utils/core.py
@@ -19,3 +19,13 @@ class BaseDistributionHandler(ABC):
     @staticmethod
     def rngen(n, th, size=1):
         raise NotImplementedError
+
+    @staticmethod
+    def ubound(n, l0, th0, th, size=1):
+        raise NotImplementedError
+    @staticmethod
+    def lbound(n, l1, th1, th, size=1):
+        raise NotImplementedError
+    @staticmethod
+    def hbound(l0, l1, th0, th1, th, size=1):
+        raise NotImplementedError
